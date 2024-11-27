87a2fc55759151fcbc80419061430b1cac072862
yaewon94 <yaewon94@naver.com>
2024-11-27 17:20:01 +0900

    Transform : 방향벡터 필드 추가, 값이 바뀌었을때만 월드행렬, 방향벡터 갱신하도록 수정

--- Project/Engine/Transform.cpp
+++ Project/Engine/Transform.cpp
@@ -3,15 +3,23 @@
 #include "Device.h"
 #include "ConstBuffer.h"
 
+// 방향 단위벡터
+static const array<const Vec3, DIRECTION_TYPE_COUNT_END> UNIT_VEC 
+	= {Vec3(1.f, 0.f, 0.f)
+	, Vec3(0.f, 1.f, 0.f)
+	, Vec3(0.f, 0.f, 1.f)};
+
 Transform::Transform(GameObject* const owner) 
 	: Component(owner)
 	, m_localScale(Vec3(100.f, 100.f, 1.f))
+	, m_isChangedValue(0)
 {
 }
 
 Transform::Transform(const Transform& origin, GameObject* const newOwner) 
 	: Component(origin, newOwner)
 	, m_localPos(origin.m_localPos), m_localScale(origin.m_localScale), m_localRotation(origin.m_localRotation)
+	, m_isChangedValue(0)
 {
 }
 
@@ -19,16 +27,36 @@ Transform::~Transform()
 {
 }
 
-void Transform::FinalTick()
+void Transform::Init()
 {
 	m_matWorld = XMMatrixIdentity();
+}
 
-	Matrix matScale = XMMatrixScaling(m_localScale.x, m_localScale.y, m_localScale.z);
-	Matrix matRotation = XMMatrixRotationX(m_localRotation.x) * XMMatrixRotationY(m_localRotation.y) * XMMatrixRotationZ(m_localRotation.z);
-	Matrix matTrans = XMMatrixTranslation(m_localPos.x, m_localPos.y, m_localPos.z);
+void Transform::FinalTick()
+{
+	if (m_isChangedValue | IS_CHANGED_VALUE)
+	{
+		// 월드행렬 갱신
+		Matrix matScale = XMMatrixScaling(m_localScale.x, m_localScale.y, m_localScale.z);
+		Matrix matRotation = XMMatrixRotationX(m_localRotation.x) * XMMatrixRotationY(m_localRotation.y) * XMMatrixRotationZ(m_localRotation.z);
+		Matrix matTrans = XMMatrixTranslation(m_localPos.x, m_localPos.y, m_localPos.z);
+		m_matWorld = matScale * matRotation * matTrans;
 
-	// 크기 * 회전 * 이동
-	m_matWorld = matScale * matRotation * matTrans;
+		if (m_isChangedValue & ON_CHANGE_ROTATION)
+		{
+			// 방향벡터 갱신
+			m_localDir[DIRECTION_TYPE::DIR_RIGHT] = Vec3(1.f, 0.f, 0.f);
+			m_localDir[DIRECTION_TYPE::DIR_UP] = Vec3(0.f, 1.f, 0.f);
+			m_localDir[DIRECTION_TYPE::DIR_FRONT] = Vec3(0.f, 0.f, 1.f);
+
+			for (byte i = 0; i < DIRECTION_TYPE_COUNT_END; ++i)
+			{
+				m_worldDir[i] = m_localDir[i] = XMVector3TransformNormal(UNIT_VEC[i], matRotation);
+			}
+		}
+
+		m_isChangedValue = 0;
+	}
 }
 
 void Transform::Binding()
--- Project/Engine/Transform.h
+++ Project/Engine/Transform.h
@@ -12,6 +12,12 @@ struct CB_Transform
 	Matrix	matWVP; // world * view * projection
 };
 
+// 방향 타입
+enum DIRECTION_TYPE : byte
+{
+	DIR_RIGHT, DIR_UP, DIR_FRONT, DIRECTION_TYPE_COUNT_END
+};
+
 extern CB_Transform g_tr;
 
 // 게임오브젝트 좌표, 크기, 각도 정보
@@ -24,15 +30,48 @@ private:
 	Vec3 m_localPos, m_localScale, m_localRotation;
 	Matrix m_matWorld;
 
+	array<Vec3, DIRECTION_TYPE_COUNT_END> m_localDir, m_worldDir;
+
+private:
+	enum CHANGE_VALUE : byte
+	{
+		ON_CHANGE_POS_SCALE = 1,
+		ON_CHANGE_ROTATION = ON_CHANGE_POS_SCALE << 1,
+
+		IS_CHANGED_VALUE = ON_CHANGE_POS_SCALE | ON_CHANGE_ROTATION
+	};
+
+	byte m_isChangedValue; 	// CHANGE_VALUE 값들 | 연산으로 조합
+
 public:
 	Transform(GameObject* const owner);
 	Transform(const Transform& origin, GameObject* const newOwner);
 	~Transform();
-public:
+
+private: // GameObject : Component* 를 통해 호출
+	virtual void Init() final;
 	virtual void FinalTick() final;
 
 public:
 	void Binding();
+
+public:
+	Vec3 GetLocalPos() const { return m_localPos; }
+	void SetLocalPos(const Vec3& pos) { m_localPos = pos; m_isChangedValue |= ON_CHANGE_POS_SCALE; }
+
+	Vec3 GetLocalScale() const { return m_localScale; }
+	void SetLocalScale(const Vec3& scale) { m_localScale = scale; m_isChangedValue |= ON_CHANGE_POS_SCALE; }
+
+	Vec3 GetLocalRotation() const { return m_localRotation; }
+	void SetLocalRotation(const Vec3& rotation) { m_localRotation = rotation; m_isChangedValue |= ON_CHANGE_ROTATION; }
+
+	const Vec3& GetWorldDirection(DIRECTION_TYPE type)
+	{
+#ifdef _DEBUG
+		if (type >= DIRECTION_TYPE_COUNT_END) assert(nullptr);
+#endif // _DEBUG
+		return m_worldDir[type];
+	}
 
 private:
 	virtual Transform* Clone(GameObject* const newOwner) final { return new Transform(*this, newOwner); }
