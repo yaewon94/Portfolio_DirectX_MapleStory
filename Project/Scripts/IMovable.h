0779cbb393b32ad261c6a97cd2eac9d5b8eeb075
yaewon94 <yaewon94@naver.com>
2024-12-17 13:24:03 +0900

    IMovable 클래스 추가 : Player 클래스의 이동 관련 필드, 메소드 IMovable 클래스로 이관

---
+++ Project/Scripts/IMovable.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "pch.h"
+
+class IMovable
+{
+	NO_COPY_MOVE(IMovable)
+
+protected:
+	// 이동 방향
+	enum MOVE_DIRECTION : int
+	{
+		LEFT = -1, RIGHT = 1
+	};
+
+protected:
+	float m_moveSpeed;
+	MOVE_DIRECTION m_moveDir;
+
+protected:
+	IMovable() 
+		: m_moveDir(MOVE_DIRECTION::RIGHT), m_moveSpeed(300.f) {}
+	IMovable(float moveSpeed) 
+		: m_moveDir(MOVE_DIRECTION::RIGHT), m_moveSpeed(moveSpeed) {}
+	virtual ~IMovable() {}
+
+protected:
+	inline virtual void Move(MOVE_DIRECTION dir) = 0;
+
+public:
+	float GetMoveSpeed() const { return m_moveSpeed; }
+	void SetMoveSpeed(float speed) { m_moveSpeed = speed; }
+};
