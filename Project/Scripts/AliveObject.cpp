abab6e33f7c4e1aa33d8e7f4e974e3e353b504c2
yaewon94 <yaewon94@naver.com>
2024-12-20 23:03:26 +0900

    AliveObject : 현재체력, 최대체력, 공격력 필드 추가

--- Project/Scripts/AliveObject.cpp
+++ Project/Scripts/AliveObject.cpp
@@ -2,10 +2,10 @@
 #include "AliveObject.h"
 #include "Engine/GameObject.h"
 #include "Engine/FlipbookPlayer.h"
-#include "Engine/FSM.h"
 
 AliveObject::AliveObject(GameObject* const owner) 
 	: Script(owner)
+	, m_maxHP(100), m_curHP(m_maxHP), m_power(100)
 	, m_flipbookPlayer(nullptr), m_fsm(nullptr)
 {
 }
@@ -13,6 +13,7 @@ AliveObject::AliveObject(GameObject* const owner)
 AliveObject::AliveObject(const AliveObject& origin, GameObject* const newOwner) 
 	: Script(origin, newOwner)
 	, IMovable(origin.m_moveSpeed)
+	, m_maxHP(origin.m_maxHP), m_curHP(m_maxHP), m_power(origin.m_power)
 {
 	m_flipbookPlayer = GetOwner()->GetComponent<FlipbookPlayer>();
 	m_fsm = GetOwner()->GetComponent<FSM>();
--- Project/Scripts/AliveObject.h
+++ Project/Scripts/AliveObject.h
@@ -2,10 +2,17 @@
 #include "Engine/Script.h"
 #include "IMovable.h"
 
+typedef UINT HP;
+typedef HP Power;
+
 // 플레이어, 몬스터 등 상태를 가지고 있고, 이동가능, 스킬사용가능 등의 특징을 가진 오브젝트
 class AliveObject : public Script, public IMovable
 {
 	NO_COPY_MOVE(AliveObject)
+
+private:
+	HP m_maxHP, m_curHP;
+	Power m_power;
 
 protected:
 	class FlipbookPlayer* m_flipbookPlayer;
@@ -20,6 +27,39 @@ protected: // GameObject : Component* 를 통해 호출
 	virtual void Init() override;
 
 public:
-	FlipbookPlayer* const GetFlipbookPlayer() const { return m_flipbookPlayer; }
 	FSM* const GetFSM() const { return m_fsm; }
+	FlipbookPlayer* const GetFlipbookPlayer() const { return m_flipbookPlayer; }
+	Power GetPower() const { return m_power; }
+
+protected:
+	void ChangeHP(HP changeAmount);
 };
+
+////////////////////////////////////////////////////
+// AliveObject.inl
+////////////////////////////////////////////////////
+#include "Engine/FSM.h"
+inline void AliveObject::ChangeHP(HP changeAmount)
+{
+	// 현재 체력에 반영
+	// TODO : HP 타입을 signed 로 바꿀 경우 overflow, underflow 조건체크코드 바꿔야 함
+	if (m_curHP + changeAmount < m_curHP) // overflow
+	{
+		m_curHP = m_maxHP;
+	}
+	else if (m_curHP + changeAmount > m_curHP) // underflow
+	{
+		m_curHP = 0;
+	}
+	else
+	{
+		m_curHP += changeAmount;
+		if (m_curHP > m_maxHP) m_curHP = m_maxHP;
+	}
+
+	// 현재체력 0
+	if (m_curHP == 0)
+	{
+		m_fsm->ChangeState(STATE_TYPE::DEAD);
+	}
+}
